1f70023d6ff382a456073a58de03b5fbcc89a6ce
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-09-21 18:48:28 +0300

    CUB-P1 setup express to work with static files, trim handlebars views

--- ExpressJS-and-Templating - Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ ExpressJS-and-Templating - Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
+const path = require("path");
 
 const app = express();
 const PORT = 5050;
@@ -8,6 +9,11 @@ const PORT = 5050;
 app.engine("hbs", handlebars.engine({ extname: "hbs" }));
 app.set("view engine", "hbs");
 app.set("views", "src/views");
+
+// Setup static files
+// const staticFiles = express.static("src/public");
+const staticFiles = express.static(path.resolve(__dirname, "public"));
+app.use(staticFiles);
 
 app.get("/", (req, res) => {
   res.render("index");
