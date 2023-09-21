e081d9a807d2c72afe3f65e1d83a436ba82d250d
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-09-21 18:39:10 +0300

    CUB-P1 initialize handlebars, setup first default view

--- ExpressJS-and-Templating - Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ ExpressJS-and-Templating - Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+
 const app = express();
 const PORT = 5050;
 
+// Handlebars configuration
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
 app.get("/", (req, res) => {
-  res.send("Hello from express server!");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
