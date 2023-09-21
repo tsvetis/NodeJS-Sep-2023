7306df117ee662be6312c5728dce44cce3ede889
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-09-21 21:31:15 +0300

    CUB-P1 remove default cubes

--- ExpressJS-and-Templating - Workshop/03. Cubicle-Workshop-Part-1-Resources/src/services/cubeService.js
+++ ExpressJS-and-Templating - Workshop/03. Cubicle-Workshop-Part-1-Resources/src/services/cubeService.js
@@ -1,29 +1,5 @@
 const uniqid = require("uniqid");
-const cubes = [
-  {
-    id: "un0jndnqlmtg3mgn",
-    name: "Kotkata Pesho",
-    description: "asd",
-    imageUrl:
-      "https://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg",
-    difficultyLevel: 1,
-  },
-  {
-    id: "un0jndnqlmtg3rm3",
-    name: "sad",
-    description: "asd",
-    imageUrl: "asd",
-    difficultyLevel: 1,
-  },
-  {
-    id: "un0jndnqlmtg4are",
-    name: "cube3",
-    description: "n/a",
-    imageUrl:
-      "https://images.pexels.com/photos/1500610/pexels-photo-1500610.jpeg?cs=srgb&dl=pexels-jadson-thomas-1500610.jpg&fm=jpg",
-    difficultyLevel: 3,
-  },
-];
+const cubes = [];
 
 exports.create = (cubeData) => {
   const newCube = {
