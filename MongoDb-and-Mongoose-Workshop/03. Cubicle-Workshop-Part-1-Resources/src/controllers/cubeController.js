e5508e5b1288026ecdebbd97fbbee1ec402e8396
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-09-28 20:40:46 +0300

    use accessoryService in cubeController and render in dropdown

--- MongoDb-and-Mongoose-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/controllers/cubeController.js
+++ MongoDb-and-Mongoose-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/controllers/cubeController.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const cubeService = require("../services/cubeService");
+const accessoryService = require("../services/accessoryService");
 
 router.get("/create", (req, res) => {
   res.render("cube/create");
@@ -27,6 +28,15 @@ router.get("/:cubeId/details", async (req, res) => {
   }
 
   res.render("cube/details", { cube });
+});
+
+router.get("/:cubeId/attach-accessory", async (req, res) => {
+  const { cubeId } = req.params;
+  const cube = await cubeService.getSingleCube(cubeId).lean();
+  const accessories = await accessoryService.getAll().lean();
+  const hasAccessories = accessories.length > 0;
+
+  res.render("accessory/attach", { cube, accessories, hasAccessories });
 });
 
 module.exports = router;
--- MongoDb-and-Mongoose-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/services/accessoryService.js
+++ MongoDb-and-Mongoose-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/services/accessoryService.js
@@ -1,3 +1,5 @@
 const Accessory = require("./../models/Accessory");
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
+
+exports.getAll = () => Accessory.find();
