9e0ecdbecba4bf9700e788744aad2d6fa7f397c0
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-09-28 18:30:49 +0300

    extract connection string in the config, connect app to the db

--- MongoDb-and-Mongoose-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
+++ MongoDb-and-Mongoose-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/index.js
@@ -1,7 +1,10 @@
 // Imports
 const express = require("express");
+
 const handlebarsConfig = require("./config/handlebarsConfig");
 const expressConfig = require("./config/expressConfig");
+const dbConnect = require("./config/dbConfig");
+
 const { PORT } = require("./constants");
 const routes = require("./router");
 
@@ -11,6 +14,11 @@ const app = express();
 // Configs
 expressConfig(app);
 handlebarsConfig(app);
+
+// Connecting to the database
+dbConnect()
+  .then(() => console.log("Successfully connected to the DB!"))
+  .catch((err) => console.log(`Error while connecting in DB: ${err}`));
 
 // Routing
 app.use(routes);
