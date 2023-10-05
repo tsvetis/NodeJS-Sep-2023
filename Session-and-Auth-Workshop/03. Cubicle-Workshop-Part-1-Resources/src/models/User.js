9976c0a807d40596ca6afdf0a9e30c172e2db00d
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-10-05 19:19:28 +0300

    hash password in the model before saving in db

--- Session-and-Auth-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/models/User.js
+++ Session-and-Auth-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
   username: String,
@@ -10,6 +11,11 @@ userSchema.virtual("repeatPassword").set(function (value) {
   if (value !== this.password) {
     throw new mongoose.MongooseError("Password missmatch!");
   }
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
 });
 
 const User = mongoose.model("User", userSchema);
