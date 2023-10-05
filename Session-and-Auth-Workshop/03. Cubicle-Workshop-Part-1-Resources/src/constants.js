9254004535db95eff42d761a070ddc1776972674
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-10-05 19:46:50 +0300

    provide secret for jwt

--- Session-and-Auth-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/constants.js
+++ Session-and-Auth-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/constants.js
@@ -2,6 +2,7 @@ const constants = {
   PORT: 5050,
   //              localhost = 127.0.0.1
   URL: "mongodb://localhost:27017/cubical-sep-2023", // Connection String
+  SECRET: "e1c22f91-8048-48d5-bda8-6df6897aa325",
 };
 
 module.exports = constants;
