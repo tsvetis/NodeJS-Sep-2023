90aa572deb869369fb8ce03db64980693fc2fb93
Tsveti Stoyanov <tsveti.stoyanov@gmail.com>
2023-10-05 21:12:54 +0300

    provide global variables for hbs template with res.locals

--- Session-and-Auth-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/middlewares/authMiddleware.js
+++ Session-and-Auth-Workshop/03. Cubicle-Workshop-Part-1-Resources/src/middlewares/authMiddleware.js
@@ -8,6 +8,8 @@ exports.auth = async (req, res, next) => {
     try {
       const decodedToken = await jwt.verify(token, SECRET);
       req.user = decodedToken;
+      res.locals.user = decodedToken;
+      res.locals.isAuthenticated = true;
 
       next();
     } catch (error) {
